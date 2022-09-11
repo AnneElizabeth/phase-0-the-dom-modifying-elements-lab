53c942b8174b55c2acd628fe87be799ace6e5884
Anne <edmondson.anne@gmail.com>
2022-09-11 13:31:51 -0600

    commit tree 5ca5fc751a0e8f3c59af2eedd91b2eee01e5a6f1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const removeMain = document.getElementById("main");
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append("newHeader");
+
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML="YOUR-NAME is the champion";
+
